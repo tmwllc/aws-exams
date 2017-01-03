ad5d596f8874a2e1c9d47e28e9dba9fce271c6a1
Tom <tomwallace75@hotmail.com>
2017-01-02 23:24:42 -0800

    Converted tabs to spaces

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express'),
-	app = express(),
-	path = require('path');
+  app = express(),
+  path = require('path');
 
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname, 'public', 'certified-developer-associate.html'));
+  res.sendFile(path.join(__dirname, 'public', 'certified-developer-associate.html'));
 });
 
 app.listen(3000, function() {
-	console.log('Example app listening on port 3000');
+  console.log('Example app listening on port 3000');
 });
