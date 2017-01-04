1140f944a42af2e0f29215cf213b8669725f9dcb
Tom <tomwallace75@hotmail.com>
2017-01-03 18:35:24 -0800

    Added error handling for routes.

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public', 'certified-developer-associate.html'));
 });
 
+app.use(function(req, res) {
+  res.status(404).send('File Not Found');
+});
+
+app.use(function(error, req, res) {
+  console.error(error.stack);
+  res.status(505).send('Internal Server Error');
+});
+
 app.listen(3000, function() {
   console.log('Example app listening on port 3000');
 });
