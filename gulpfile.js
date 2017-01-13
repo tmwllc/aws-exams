ae30b0ef0ba21c4a8bb4db1c8ba4c8e10d0d553a
Tom <tomwallace75@hotmail.com>
2017-01-12 21:35:19 -0800

    Added gulpfile which runs server with browserSync to load Chrome and watch sass files to automatically compile css and inject styles to browser.

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  nodemon = require('gulp-nodemon'),
+  browserSync = require('browser-sync');
+
+gulp.task('styles',  function() {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('nodemon', function(cb) {
+  var started = false;
+
+  return nodemon({
+    script: 'app.js'
+  }).on('start', function() {
+    if (!started) {
+      cb();
+      started = true;
+    }
+  });
+});
+
+gulp.task('browser-sync', ['nodemon', 'styles'], function() {
+  gulp.watch('sass/**/*.scss', ['styles']);
+  browserSync.init(null, {
+    proxy: 'http://localhost:3000',
+    files: ['public/*&*/*.*'],
+    browser: 'chrome',
+    port: 7000
+  });
+});
+
+gulp.task('default', ['browser-sync']);
